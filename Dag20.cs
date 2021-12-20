70efbb309123a884f43013bb05f49a0b9bb11cd1
FDGRoelofs <floris_roelofs@hotmail.com>
2021-12-20 21:53:11 +0100

    dag 20 werkt nog niet

---
+++ Dag20.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021
+{
+    internal class Dag20 : Dag
+    {
+        public Dag20(string s) : base(s)
+        {
+
+        }
+        public bool[] enhancementkey = new bool[512];
+        public bool[,] inputmap;
+        public bool[,] outputmap;
+        public int dimension;
+
+        public override void Puzzel1()
+        {
+            string enhancementstring = lines[0];
+            dimension = lines.Length + 3;
+            inputmap = new bool[dimension, dimension];
+            outputmap = new bool[dimension, dimension];
+            for (int i = 0; i < enhancementstring.Length; i++)
+            {
+                if(enhancementstring[i] == '#')
+                    enhancementkey[i] = true;
+            }
+            InitializeInputmap();
+            this.result2 = getSegmentValue(5, 5).ToString();
+            Enhance();
+            inputmap = outputmap;
+            outputmap = new bool[dimension, dimension];
+            //voor debug redenen
+            int answer = 0;
+            for (int y = 0; y < dimension; y++)
+                for (int x = 0; x < dimension; x++)
+                    if (inputmap[x, y])
+                        answer++;
+            //this.result2 = answer.ToString();
+            
+            //tot hier*/
+            Enhance();
+            answer = 0;
+            for (int y = 0; y < dimension; y++)
+                for (int x = 0; x < dimension; x++)
+                    if (outputmap[x, y])
+                        answer++;
+            this.result1 = answer.ToString();
+        }
+
+        public override void Puzzel2()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void InitializeInputmap()
+        {
+            for (int y = 2; y < lines.Length; y++)
+                for (int x = 2; x < lines[y].Length + 2; x++)
+                    if (lines[y][x - 2] == '#')
+                        inputmap[x,y] = true;
+        }
+
+        public bool getpixelatcoord(int x, int y)
+        {
+            if(y < 0 || y >= dimension)
+                return false;
+            if(x < 0 || x >= dimension)
+                return false;
+            return inputmap[x,y];
+        }
+
+        public void Enhance()
+        {
+            for(int x = 0; x < dimension; x++)
+                for(int y = 0; y < dimension; y++)
+                {
+                    outputmap[x,y] = enhancementkey[getSegmentValue(x,y)];
+                }
+        }
+
+        public int getSegmentValue(int x, int y)
+        {
+            int value = 0;
+            if (getpixelatcoord(x - 1, y - 1))
+                value += 0b_10000000;
+            if (getpixelatcoord(x, y - 1))
+                value += 0b_01000000;
+            if (getpixelatcoord(x + 1, y - 1))
+                value += 0b_001000000;
+            if (getpixelatcoord(x - 1, y))
+                value += 0b_000100000;
+            if (getpixelatcoord(x, y))
+                value += 0b_000010000;
+            if (getpixelatcoord(x + 1, y))
+                value += 0b_000001000;
+            if (getpixelatcoord(x - 1, y + 1))
+                value += 0b_000000100;
+            if (getpixelatcoord(x, y + 1))
+                value += 0b_000000010;
+            if (getpixelatcoord(x + 1, y + 1))
+                value += 0b_000000001;
+            return value;
+        }
+    }
+}
